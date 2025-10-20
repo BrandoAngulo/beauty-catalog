d415bf7ab186dc4dc38a079294bca9c1588ba321
bangulo <bangulo@playtechla.com>
2025-10-20 15:38:30 -0500

    se agregan las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { CatalogPagesComponent } from './pages/catalog-pages/catalog-pages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: CatalogPagesComponent,
+        title: 'Catálogo de de belleza',
+    },
+    {
+        // Redirige cualquier ruta no encontrada a la página principal
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full',
+    },
+
+];
